ab05a225cce427221b7c8eb440306a556955a73b
jhons007 <jhons_aiorus@hotmail.com>
2016-07-23 13:16:37 -0500

    modificando funciones modificar iliminar y reporte
    esta compilando al 100%

--- ISUR-PROGRAMA.py
+++ ISUR-PROGRAMA.py
@@ -1,11 +1,117 @@
-TRABAJO FINAL INSTITUTO DEL SUR import sqlite3 import os import sys def nuevo(): nombre = input("ingrese su nombre ") edad = input("ingrese su edad ") dni = input("ingrese su DNI ") con = sqlite3.connect('tienda.s3db') cursor = con.cursor() cursor.execute("insert into cursos (nombre,edad,dni) values ('"+nombre+"','"+edad+"','"+dni+"')") con.commit() input("operacion completada") con.close() def modificar(): persona=[] con = sqlite3.connect('tienda.s3db') cursor = con.cursor() cursor.execute('SELECT * FROM cursos') print("personas agregados") print("\t \t \t \ ") print("---------------------------------------------------------") for persona in cursor: cursos.append(persona) nom = '\t '+str(persona[0])+ '\t '+persona[1]+'\t '+persona[2]+ '\t '+persona[3] print(nom) cod=input("digite el codigo") encontrado =False for persona in cursos: if int(persona[0]) ==int(cod): nombre=persona[1] edad=persona[2] dni=persona[3] encontrado == True break else: input("digite un opcion porfavor..") os.system("clear") modificar() nombre=input("digite el nuevo nombre de "+nombre+":") edad=input("digite el nuevo nombre de "+edad+":") dni=input("digite el nuevo nombre de "+dni+":")
+import os
+import sys,time
+import sqlite3
 
-sql="update cursos set nombre-'"+nombre+"',edad-'"+edad+"',dni-'"+dni+"' where cod-"+cod
-cursor.execute(sql)
-con.commit()
-con.close()
-print("modificado......<->")
-input()
-def iliminar(): con = sqlite3.connect('tienda.s3db') cursor = con.cursor() cursor.execute('SELECT * FROM cursos') print("personas agregados") print("\t \t \t \ ") print("-------------------------------------------------") for persona in cursor: cursos.append(persona) nom = '\t '+str(persona[0])+ '\t '+persona[1]+'\t '+persona[2]+ '\t '+persona[3] print(nom) cod=input("digite el codigo") if cod==True: return True else: input("codigo incorrecto") iliminar() sql="delete persona where cod-"+cod cursor.execute(sql) con.commit() con.close() print("iliminado......<->") input()
+def nuevo():
+    os.system("cls")
+    nombre=input("digite el nombre del alumno")
+    edad=input("digire la edad del alumno")
+    dni=input("digite el dni del alumno")
+    con=sqlite3.connect("trabajo final.s3db")
+    cursor=con.cursor()
+    cursor.execute("insert into instituto (nombre,edad,dni) values ('"+nombre+"','"+edad+"','"+dni+"')")
+    con.commit()
+    con.close()
 
-def indice(): print("1.-agregar nombre del alumno ") print("2.-modificar nombre del alumno") print("3.-iliminar nombre") print("4.-salir") print("") rvl=input("Digite una opcion") if rvl=="1": nuevo() elif rvl=="2": modificar() elif rvl=="3": iliminar() elif rvl=="4": input("este program se esta cerrndo ...") sys.exit() else: input("ingrese una opcion valida") indice() indice()
+def menu():
+    os.system("cls")
+    print("Base de datos del Instituto del Sur")
+    print("")
+    print("1.- agregar articulo")
+    print("2.- ver alumno")
+    print("3.- modificar articulo")
+    print("4.- iliminar articulo")
+    print("5.- salir")
+    while(1):
+        try:
+            print("ingrese una opcion")
+            entrada=input()
+            entrada=int(entrada)
+            while(entrada<0 or entrada>5):
+                print("ingrese correctamente la opcion")
+                entrada=input()
+                entrada=int(entrada)
+            break
+        except ValueError:
+            print("ingrese correctamente una opcion valida")
+    if (entrada==1):
+        nuevo()
+    elif(entrada==2):
+        reporte()
+    elif(entrada==3):
+        modificar()
+    elif(entrada==4):
+        iliminar()
+    elif(entrada==5):
+        sys.exit()
+
+def reporte():
+    con=sqlite3.connect("trabajo final.s3db")
+    cursor=con.cursor()
+    cursor.execute("select*from instituto ")
+    for instituto in cursor:
+        print("")
+        print("\t alumnos agregados")
+        print("\t------------------------")
+        print("\t nombre:"'\t'+str(instituto[1]))
+        print("\t edad:"'\t'+str(instituto[2]))
+        print("\t dni:"'\t'+str(instituto[3]))
+        print("\t codigo:"'\t'+str(instituto[0]))
+    con.commit()
+    con.close()
+    input()
+    menu()
+def modificar():
+    con=sqlite3.connect("trabajo final.s3db")
+    cursor=con.cursor()
+    cursor.execute("select*from instituto ")
+    for instituto in cursor:
+        print("")
+        print("\t alumnos agregados")
+        print("\t------------------------")
+        print("\t nombre:"'\t'+str(instituto[1]))
+        print("\t edad:"'\t'+str(instituto[2]))
+        print("\t dni:"'\t'+str(instituto[3]))
+        print("\t codigo:"'\t'+str(instituto[0]))
+    cod=input("digite el codigo del articulo que desea modificar")
+    for intituto in cursor:
+        if int(instituto[0])==int(cod):
+            nombre=instituto[1]
+            edad=instituto[2]
+            dni=instituto[3]
+            break
+    nombre= input("Digite nuevo nombre o corrija" )
+    edad=input("Digite nueva edad o corrija")
+    dni= input("Digite nuevo dni o corrija" )
+    sql = "UPDATE instituto set nombre ='"+nombre+"', edad='"+edad+"',dni='"+dni+"' where codigo = "+cod
+    cursor.execute(sql)
+
+    con.commit()
+    con.close()
+    print("")
+    print("el archivo fue modificado exitoxamente-.......")
+    input()
+    menu()
+def iliminar():
+    con=sqlite3.connect("trabajo final.s3db")
+    cursor=con.cursor()
+    cursor.execute("select*from instituto ")
+    for instituto in cursor:
+        print("")
+        print("\t alumnos agregados")
+        print("\t------------------------")
+        print("\t nombre:"'\t'+str(instituto[1]))
+        print("\t edad:"'\t'+str(instituto[2]))
+        print("\t dni:"'\t'+str(instituto[3]))
+        print("\t codigo:"'\t'+str(instituto[0]))
+    cod=input("digite el codigo del articulo que desea iliminar")
+    sql="delete from instituto where codigo="+cod
+    cursor.execute(sql)
+    con.commit()
+    con.close()
+    print("")
+    print("el archivo fue iliminado exitoxamente-.......")
+    input()
+    menu()
+
+menu()
